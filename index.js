bdb7b1434d97ade9f6a88dc02e331ae9b89d3bb8
KennedyMelo <kennedycmelo@gmail.com>
2023-07-08 23:06:35 -0300

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const MercadoPago = require("mercadopago")
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 MercadoPago.configure({
   sandbox: true,
